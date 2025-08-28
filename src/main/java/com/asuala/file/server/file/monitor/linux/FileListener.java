18b6386788789296289dd9c09dec18c3f3c6432d
asuala <1214146598@qq.com>
2025-08-28 09:13:36 +0800

    循环判断修改

--- src/main/java/com/asuala/file/server/file/monitor/linux/FileListener.java
+++ src/main/java/com/asuala/file/server/file/monitor/linux/FileListener.java
@@ -37,18 +37,30 @@ public class FileListener {
     public static ExecutorService fixedThreadPool;
 
 
+    // 提取获取父目录ID的公共方法
+    private Long getParentDirId(FileVo poll) {
+        FileMemory fileMemory = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath());
+        if (null == fileMemory) {
+            FileInfo parentInfo = fileInfoService.findFileInfo(new File(poll.getParentPath()));
+            return parentInfo != null ? parentInfo.getDId() : -1;
+        } else {
+            return fileMemory.getDId();
+        }
+    }
     @PostConstruct
     public void consumer() {
         fixedThreadPool = Executors.newFixedThreadPool(1);
         fixedThreadPool.execute(() -> {
             while (CacheUtils.watchFlag) {
-                FileVo poll = (FileVo) CacheUtils.queue.poll();
+                FileVo poll = new FileVo();
                 try {
-                    if (null == poll) {
-                        Thread.sleep(1000L);
-                    } else {
-                        int mask = poll.getCode();
-                        boolean isDir = poll.isDir();
+                    while (CacheUtils.watchFlag) {
+                        poll = (FileVo) CacheUtils.queue.poll();
+                        if (null == poll) {
+                            Thread.sleep(1000L);
+                        } else {
+                            int mask = poll.getCode();
+                            boolean isDir = poll.isDir();
 
 //                        if (!isDir) {
 //                            if (poll.getName().endsWith("~")) {
@@ -63,87 +75,88 @@ public class FileListener {
 //                                continue;
 //                            }
 //                        }
-                        FileMemory fileMemory;
-                        Long pId;
-                        FileInfo fileInfo;
-                        FileInfo insert;
-                        switch (mask) {
-                            case Constant.IN_CREATE:
-                                log.debug("文件删除 {}",poll.getFullPath());
-                                fileMemory = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath());
-                                if (null == fileMemory) {
-                                    pId = fileInfoService.findFileInfo(new File(poll.getParentPath())).getDId();
-                                } else {
-                                    pId = fileMemory.getDId();
-                                }
-                                insert = fileInfoService.insert(new File(poll.getFullPath()), poll.getSId(), pId);
-                                InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().put(poll.getFullPath(), FileMemory.builder().dId(insert.getDId()).id(insert.getId()).build());
-
-                                break;
-                            case Constant.IN_MOVED_TO:
-                                log.debug("文件移入 {}",poll.getFullPath());
-                                fileMemory = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath());
-                                if (null == fileMemory) {
-                                    pId = fileInfoService.findFileInfo(new File(poll.getParentPath())).getDId();
-                                } else {
-                                    pId = fileMemory.getDId();
-                                }
-                                if (poll.isDir()) {
-                                    fileInfoService.batchSave(InotifyLibraryUtil.findDirFile(poll.getFullPath(), poll.getSId(), poll.getFd(), pId), poll.getName(), poll.getFd());
-                                } else {
+                            FileMemory fileMemory;
+                            Long pId;
+                            FileInfo fileInfo;
+                            FileInfo insert;
+                            switch (mask) {
+                                case Constant.IN_CREATE:
+                                    log.debug("文件删除 {}", poll.getFullPath());
+                                    fileMemory = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath());
+                                    if (null == fileMemory) {
+                                        pId = fileInfoService.findFileInfo(new File(poll.getParentPath())).getDId();
+                                    } else {
+                                        pId = fileMemory.getDId();
+                                    }
                                     insert = fileInfoService.insert(new File(poll.getFullPath()), poll.getSId(), pId);
                                     InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().put(poll.getFullPath(), FileMemory.builder().dId(insert.getDId()).id(insert.getId()).build());
-                                }
-                                break;
-                            case Constant.IN_MODIFY:
-                                log.debug("文件修改 {}",poll.getFullPath());
-                                if (!isDir) {
-                                    fileInfo = fileInfoService.findFileInfo(poll);
-                                    if (null == fileInfo) {
-                                        pId = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath()).getId();
-                                        log.warn("{} 修改文件事件-没有文件", poll.getFullPath());
-                                        fileInfoService.insert(new File(poll.getFullPath()), poll.getSId(), pId);
-                                    } else {
-                                        File file = new File(poll.getFullPath());
-                                        fileInfo.setSize(file.length());
-                                        fileInfo.setChangeTime(new Date(file.lastModified()));
-                                        fileInfo.setUpdateTime(new Date());
-                                        fileInfoService.updateById(fileInfo);
-                                        esService.saveEs(fileInfo);
-                                    }
-                                }
-                                break;
-                            case Constant.IN_MOVED_FROM:
-                                log.debug("文件移出 {}",poll.getFullPath());
-                                fileInfo = fileInfoService.findFileInfo(poll);
-                                if (null != fileInfo) {
-                                    InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().remove(poll.getFullPath());
 
-                                    fileInfoService.deleteByPrimaryKey(fileInfo.getId());
-                                    esService.delEs(fileInfo);
-                                    if (poll.isDir()) {
-                                        InotifyLibraryUtil.fdMap.get(poll.getFd()).removeBidi(poll.getFullPath());
-                                        fileInfoService.clearDir(fileInfo, poll);
+                                    break;
+                                case Constant.IN_MOVED_TO:
+                                    log.debug("文件移入 {}", poll.getFullPath());
+                                    fileMemory = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath());
+                                    if (null == fileMemory) {
+                                        pId = fileInfoService.findFileInfo(new File(poll.getParentPath())).getDId();
+                                    } else {
+                                        pId = fileMemory.getDId();
                                     }
-                                }
-                                break;
-                            case Constant.IN_DELETE_SELF:
-                                log.debug("自我删除 {}",poll.getFullPath());
-                                InotifyLibraryUtil.removeWd(poll.getFd(), poll.getFullPath());
-                            case Constant.IN_DELETE:
-                                log.debug("删除 {}",poll.getFullPath());
-                                fileInfo = fileInfoService.findFileInfo(poll);
-                                if (null != fileInfo) {
-                                    InotifyLibraryUtil.removePathId(poll.getFd(), poll.getFullPath());
-                                    fileInfoService.deleteByPrimaryKey(fileInfo.getId());
-                                    esService.delEs(fileInfo);
-                                }
-                                break;
+                                    if (poll.isDir()) {
+                                        fileInfoService.batchSave(InotifyLibraryUtil.findDirFile(poll.getFullPath(), poll.getSId(), poll.getFd(), pId), poll.getName(), poll.getFd());
+                                    } else {
+                                        insert = fileInfoService.insert(new File(poll.getFullPath()), poll.getSId(), pId);
+                                        InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().put(poll.getFullPath(), FileMemory.builder().dId(insert.getDId()).id(insert.getId()).build());
+                                    }
+                                    break;
+                                case Constant.IN_MODIFY:
+                                    log.debug("文件修改 {}", poll.getFullPath());
+                                    if (!isDir) {
+                                        fileInfo = fileInfoService.findFileInfo(poll);
+                                        if (null == fileInfo) {
+                                            pId = InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().get(poll.getParentPath()).getId();
+                                            log.warn("{} 修改文件事件-没有文件", poll.getFullPath());
+                                            fileInfoService.insert(new File(poll.getFullPath()), poll.getSId(), pId);
+                                        } else {
+                                            File file = new File(poll.getFullPath());
+                                            fileInfo.setSize(file.length());
+                                            fileInfo.setChangeTime(new Date(file.lastModified()));
+                                            fileInfo.setUpdateTime(new Date());
+                                            fileInfoService.updateById(fileInfo);
+                                            esService.saveEs(fileInfo);
+                                        }
+                                    }
+                                    break;
+                                case Constant.IN_MOVED_FROM:
+                                    log.debug("文件移出 {}", poll.getFullPath());
+                                    fileInfo = fileInfoService.findFileInfo(poll);
+                                    if (null != fileInfo) {
+                                        InotifyLibraryUtil.fdMap.get(poll.getFd()).getPathIdMap().remove(poll.getFullPath());
+
+                                        fileInfoService.deleteByPrimaryKey(fileInfo.getId());
+                                        esService.delEs(fileInfo);
+                                        if (poll.isDir()) {
+                                            InotifyLibraryUtil.fdMap.get(poll.getFd()).removeBidi(poll.getFullPath());
+                                            fileInfoService.clearDir(fileInfo, poll);
+                                        }
+                                    }
+                                    break;
+                                case Constant.IN_DELETE_SELF:
+                                    log.debug("自我删除 {}", poll.getFullPath());
+                                    InotifyLibraryUtil.removeWd(poll.getFd(), poll.getFullPath());
+                                case Constant.IN_DELETE:
+                                    log.debug("删除 {}", poll.getFullPath());
+                                    fileInfo = fileInfoService.findFileInfo(poll);
+                                    if (null != fileInfo) {
+                                        InotifyLibraryUtil.removePathId(poll.getFd(), poll.getFullPath());
+                                        fileInfoService.deleteByPrimaryKey(fileInfo.getId());
+                                        esService.delEs(fileInfo);
+                                    }
+                                    break;
 //                            case Constant.IN_IGNORED:
 //                                InotifyLibraryUtil.removeWd(poll.getFd(), poll.getFullPath());
 //                                break;
-                            default:
-                                break;
+                                default:
+                                    break;
+                            }
                         }
                     }
                 } catch (Exception e) {
